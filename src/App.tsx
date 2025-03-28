73c86f4b4dead32889aaa41490f8c0947c087470
Pranto Deb <rajprit0m@outlook.com>
2025-03-28 18:44:44 +0600

    [Redux] : [create increment decrement UI]

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@
 // import UserInfoWithUseState from "./components/UserInfoWithUseState";
 // import UserInfoWithUseReducer from "./components/UserInfoWithUseReducer";
 
-import TodoForm from "./components/todo/TodoForm";
-import TodoList from "./components/todo/TodoList";
-import TodoProvider from "./context/TodoProvider";
+// import TodoForm from "./components/todo/TodoForm";
+// import TodoList from "./components/todo/TodoList";
+// import TodoProvider from "./context/TodoProvider";
 
 function App() {
   return (
@@ -17,12 +17,22 @@ function App() {
       {/* <UserInfoWithUseState /> */}
       {/* <UserInfoWithUseReducer /> */}
 
-      <TodoProvider>
+      {/* <TodoProvider>
         <div className="flex justify-center w-full gap-10 min-h-screen">
           <TodoForm />
           <TodoList />
         </div>
-      </TodoProvider>
+      </TodoProvider> */}
+
+      <div className="flex justify-center items-center gap-7 text-2xl my-16">
+        <button className="cursor-pointer bg-green-600 px-4 py-2 text-xl rounded-2xl text-white">
+          Increment
+        </button>
+        <p>0</p>
+        <button className="cursor-pointer bg-red-600 px-4 py-2 text-xl rounded-2xl text-white">
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
